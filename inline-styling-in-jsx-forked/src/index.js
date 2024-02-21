f96f134f06e2d10732b233f9e99eb5def56040e5
Kim Seng Thai <148199726+Afterburn3@users.noreply.github.com>
2024-02-21 17:59:49 +1100

    React

---
+++ inline-styling-in-jsx-forked/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const customStyle = {
+  color: "red",
+};
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
